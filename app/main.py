0378b741be9d1c8088d86d52f4f2e028cea3c1a9
Cristoffer Roger P. Requirme <rogerreq123@gmail.com>
2026-03-17 22:02:23 +0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,16 +1,14 @@
 import sys
 import os
 
-# --- ADD THESE 3 LINES RIGHT AT THE TOP ---
-# This fixes the "No module named 'app'" error when using the VS Code Run button
 current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(current_dir)
 sys.path.insert(0, parent_dir)
 # ------------------------------------------
 
 from PySide6.QtWidgets import QApplication
-from app.ui.main_window import MainWindow      # Note: I added 'app.' here for consistency!
-from app.database.db_setup import initialize_db # Note: Added 'app.' here too!
+from app.ui.main_window import MainWindow      
+from app.database.db_setup import initialize_db 
 
 def main():
     # 1. Initialize the local database if it doesn't exist
@@ -23,7 +21,7 @@ def main():
     # 3. Launch the UI
     app = QApplication(sys.argv)
     
-    # Optional: Set global font
+    # Set global font
     font = app.font()
     font.setFamily("Segoe UI")
     app.setFont(font)
